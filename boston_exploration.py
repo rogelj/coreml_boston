be24ea59fb0c0fda0c3bf8a6e2dc2b590c445121
J Rogel <j.rogel.datascience@gmail.com>
2017-12-30 18:19:20 +0000

    Creating repo
    Creating repo

---
+++ boston_exploration.py
@@ -0,0 +1,34 @@
+"""
+This script implements a machine learning model on the
+Boston house prices dataset to predict the price of a house
+given a number of features.
+
+The model is then converted to .mlmodel file to be used in an iOS
+native app.
+
+Author: Dr J Rogel
+
+Last modified: 20171230
+"""
+
+from sklearn import datasets
+
+boston = datasets.load_boston()
+boston_df = pd.DataFrame(boston.data)
+boston_df.columns = boston.feature_names
+y = boston.target
+
+
+X = boston_df[['CRIM', 'RM']]
+X.columns = ['Crime', 'Rooms']
+X.describe()
+
+# Crime       Rooms
+# count  506.000000  506.000000
+# mean     3.593761    6.284634
+# std      8.596783    0.702617
+# min      0.006320    3.561000
+# 25%      0.082045    5.885500
+# 50%      0.256510    6.208500
+# 75%      3.647423    6.623500
+# max     88.976200    8.780000
