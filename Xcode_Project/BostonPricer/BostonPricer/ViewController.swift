0bb851ba77e9ee2a6994f3301dbd85fbe8842b43
J Rogel <j.rogel.datascience@gmail.com>
2018-02-14 08:36:04 +0000

    Filling the Picker
    Filling the Picker

--- Xcode_Project/BostonPricer/BostonPricer/ViewController.swift
+++ Xcode_Project/BostonPricer/BostonPricer/ViewController.swift
@@ -2,17 +2,26 @@
 //  ViewController.swift
 //  BostonPricer
 //
-//  Created by Jesus Rogel on 07/01/2018.
-//  Copyright © 2018 Jesus Rogel. All rights reserved.
+//  Created by J Rogel on 07/01/2018.
+//  Copyright © 2018 J Rogel. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    let crimeData = Array(stride(from: 0.1, through: 0.3, by: 0.01))
+    let roomData = Array(4...9)
+
+    @IBOutlet weak var inputPicker: UIPickerView!
+
+    @IBOutlet weak var predictButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Picker data source and delegate
+        inputPicker.dataSource = self
+        inputPicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +32,60 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        guard let inputVals = inputPredictor(rawValue: component) else {
+            fatalError("No predictor for component")
+        }
+
+        switch inputVals {
+        case .crime:
+            return crimeData.count
+        case .rooms:
+            return roomData.count
+        }
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        guard let inputVals = inputPredictor(rawValue: component) else {
+            fatalError("No predictor for component")
+        }
+
+        switch inputVals {
+        case .crime:
+            return String(crimeData[row])
+        case .rooms:
+            return String(roomData[row])
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int,
+                    inComponent component: Int) {
+        guard let inputVals = inputPredictor(rawValue: component) else {
+            fatalError("No predictor for component")
+        }
+
+        switch inputVals {
+        case .crime:
+            print(String(crimeData[row]))
+        case .rooms:
+            print(String(roomData[row]))
+        }
+
+
+    }
+}
+
+enum inputPredictor: Int {
+    case crime = 0
+    case rooms
+}
