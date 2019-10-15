d48edb492cd0341dfce841ee4d24443c0d08de62
J Rogel <jrogel.datascience@icloud.com>
2019-10-15 14:45:27 +0100

    Adding Project Starter
    Adding Project Starter

---
+++ Xcode_Project/BostonPricer_Starter/BostonPricer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BostonPricer
+//
+//  Created by Jesus Rogel on 09/10/2019.
+//  Copyright © 2019 Jesus Rogel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
