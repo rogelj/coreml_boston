bed762998a089438f5e16ce8eec1923119d72c49
J Rogel <jrogel.datascience@icloud.com>
2019-10-22 11:27:30 +0100

    Adding pickers

--- Xcode_Project/BostonPricer/BostonPricer/ContentView.swift
+++ Xcode_Project/BostonPricer/BostonPricer/ContentView.swift
@@ -9,23 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let crimeData = Array(stride(from: 0.05, through: 3.65, by: 0.1))
+    let roomData = Array(4...9)
+
+    @State var popUpVisible: Bool = false
+    @State var pickerCrime = 0.05
+    @State var pickerRoom = 4
+
     var body: some View {
         NavigationView {
             VStack {
                 Text("Boston Pricer")
                     .font(.largeTitle)
                     .multilineTextAlignment(.center)
-                    .padding()
 
-                HStack {
-                    Text("Crime Rate").padding(.trailing, 40)
-                    Text("No. Rooms").padding(.leading, 40)
+                VStack {
+                    Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("Crime")) {
+                        /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                        /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
+                    }
+                    .padding(.leading, 10)
+                    Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("No. Rooms")) {
+                        /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                        /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
+                    }
+                    .padding(.leading, 10)
                 }
 
-                Spacer()
-
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                    self.popUpVisible = true
+                }) {
                     Text("Get Prediction")
+                }
+                .alert(isPresented: self.$popUpVisible) {
+                    Alert(title: Text("Prediction"),
+                        message: Text("Prediction will be shown here."),
+                        dismissButton: .default(Text("Cool!")))
                 }
 
                 Spacer()
